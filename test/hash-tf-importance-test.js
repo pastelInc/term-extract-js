0350683db3e57e6c0a3ee0a420d711625a414435
pastelInc <keyaki765@gmail.com>
2017-02-12 22:51:10 +0900

    test: remove extra test cases

--- test/hash-tf-importance-test.js
+++ test/hash-tf-importance-test.js
@@ -5,27 +5,6 @@ test.beforeEach(t => {
   t.context.imp = new HashTFImportance()
 })
 
-test('should be sorted by nouns importance desc', t => {
-  const nImp = new Map([
-    ['水瀬伊織', 1.2],
-    ['バンダイナムコゲームス', 1],
-    ['ゲーム', 1],
-    ['アイドルマスター', 1.8],
-    ['の登場人物', 1],
-    ['765プロダクション 所属 アイドル 候補生', 1.7]
-  ])
-  const expected = [
-    ['アイドルマスター', 1.8],
-    ['765プロダクション所属アイドル候補生', 1.7],
-    ['水瀬伊織', 1.2],
-    ['バンダイナムコゲームス', 1],
-    ['ゲーム', 1],
-    ['の登場人物', 1]
-  ]
-
-  t.deepEqual(Array.from(t.context.imp.nounsImpDesc(nImp)), expected)
-})
-
 test('calicurate importance of word by temporary hash on term frequency', t => {
   const sentence = `水瀬伊織（みなせ いおり）は、バンダイナムコゲームスのゲーム『アイドルマスター』の登場人物で、765プロダクション所属アイドル候補生の一人である。`
   const expected = [
@@ -38,20 +17,6 @@ test('calicurate importance of word by temporary hash on term frequency', t => {
   ]
 
   t.deepEqual(Array.from(t.context.imp.nounImportance(sentence)), expected)
-})
-
-test('return sorted list by importance', t => {
-  const sentence = `水瀬伊織（みなせ いおり）は、バンダイナムコゲームスのゲーム『アイドルマスター』の登場人物で、765プロダクション所属アイドル候補生の一人である。`
-  const expected = [
-    ['水瀬伊織', 1],
-    ['バンダイナムコゲームス', 1],
-    ['ゲーム', 1],
-    ['アイドルマスター', 1],
-    ['の登場人物', 1],
-    ['765プロダクション所属アイドル候補生', 1]
-  ]
-
-  t.deepEqual(Array.from(t.context.imp.importance(sentence)), expected)
 })
 
 test('return single-noun number data in compound word', t => {
